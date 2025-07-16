a8dfc1540f2126963c78b5be718c8aeb7c1148e1
Ognen Manevski <ognen.manevski@gmail.com>
2025-07-16 14:50:47 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// import tailwindcss from '@tailwindcss/vite'
+// import svgr from 'vite-plugin-svgr';
+
+// // https://vite.dev/config/
+// export default defineConfig(({ command }) => {
+//   const config = {
+//     plugins: [
+//       react(),
+//       tailwindcss(),
+//       svgr(),
+//     ],
+//   }
+
+//   if (command === 'serve') {
+//     // dev
+//     config.base = '/'
+//   } else {
+//     // production
+//     config.base = '/portfolio/'
+//   }
+
+//   return config
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,22 +33,11 @@ import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [
-      react(),
-      tailwindcss(),
-      svgr(),
-    ],
-  }
-
-  if (command === 'serve') {
-    // dev
-    config.base = '/'
-  } else {
-    // production
-    config.base = '/portfolio/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+  ],
+  base: '/portfolio/',
 })
